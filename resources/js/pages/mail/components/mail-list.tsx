81b21b46c5c96c1d46e0cf159af542c1652fc4be
Hoang Kiem <kiemhd@outlook.com>
2025-10-02 00:02:36 +0700

    chore: update type and color variables

--- resources/js/pages/mail/components/mail-list.tsx
+++ resources/js/pages/mail/components/mail-list.tsx
@@ -4,7 +4,6 @@ import { formatDistanceToNow } from "date-fns/formatDistanceToNow"
 import { cn } from "@/lib/utils"
 import { Badge } from "@/components/ui/badge"
 import { ScrollArea } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
 import { Mail } from "@/pages/mail/data"
 import { useMail } from "@/pages/mail/use-mail"
 
