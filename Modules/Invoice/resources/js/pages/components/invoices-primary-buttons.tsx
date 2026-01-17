144715904c963f0fc3291942fb28e92a5744a16d
Hoang Kiem <kiemhd@outlook.com>
2026-01-17 16:32:16 +0700

    chore: update invoice report button

--- Modules/Invoice/resources/js/pages/components/invoices-primary-buttons.tsx
+++ Modules/Invoice/resources/js/pages/components/invoices-primary-buttons.tsx
@@ -1,10 +1,15 @@
 import { Link } from '@inertiajs/react'
 import { Button } from '@/components/ui/button'
-import { IconPlus } from '@tabler/icons-react'
+import { IconPlus, IconChartBar } from '@tabler/icons-react'
 
 export function InvoicesPrimaryButtons() {
   return (
     <div className='flex gap-2'>
+      <Button variant="outline" asChild>
+        <Link href={route('dashboard.invoices.reports')}>
+          <IconChartBar size={18} className='mr-1' /> Reports
+        </Link>
+      </Button>
       <Button asChild>
         <Link href={route('dashboard.invoices.create')}>
           <IconPlus size={18} className='mr-1' /> New Invoice
