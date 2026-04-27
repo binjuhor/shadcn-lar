afae3dc01a58508f4948d2ce1ea353412055f1eb
Hoang Kiem <kiemhd@outlook.com>
2026-04-27 11:40:14 +0700

    fix: blank page

--- config/inertia.php
+++ config/inertia.php
@@ -20,8 +20,8 @@ return [
     */
 
     'ssr' => [
-        'enabled' => false,
-        'url' => 'http://127.0.0.1:13714',
+        'enabled' => env('INERTIA_SSR_ENABLED', false),
+        'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
     ],
 
