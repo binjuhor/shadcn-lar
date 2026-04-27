a70987fb8ae26159e56d0b66908a8726ccece606
Hoang Kiem <kiemhd@outlook.com>
2026-04-27 14:35:30 +0700

    chore: clean

--- config/inertia.php
+++ config/inertia.php
@@ -20,7 +20,7 @@ return [
     */
 
     'ssr' => [
-        'enabled' => env('INERTIA_SSR_ENABLED', false),
+        'enabled' => false,
         'url' => env('INERTIA_SSR_URL', 'http://127.0.0.1:13714'),
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
     ],
